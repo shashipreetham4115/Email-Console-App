5242a73326b4898eae02dd2186b18492b83aa5b0
shashi-pt4501 <shashi-pt4501@csez.zohocorpin.com>
2022-02-01 17:00:46 +0530

    First Commit

--- src/ui/MailOperationsUi.kt
+++ src/ui/MailOperationsUi.kt
@@ -9,31 +9,30 @@ class MailOperationsUi : MailOperationsUiServices {
     private val delete = DeleteMailHandler()
     private val operations = MarkMailHandler()
     private val send = SendMailHandler()
-    private val email = AuthenticateUi.loggedInUser?.email!!
     private val draft = DraftMailHandler()
 
     override fun fetchFolder(folder: String): List<Mail> {
-        return fetch.fetchFolder(email, folder)
+        return fetch.fetchFolder(AuthenticateUi.loggedInUser?.email!!, folder)
     }
 
     override fun fetch(mailId: String, folder: String): List<Mail> {
-        return fetch.fetch(email, mailId, folder)
+        return fetch.fetch(AuthenticateUi.loggedInUser?.email!!, mailId, folder)
     }
 
     override fun delete(mailIds: List<String>) {
-        delete.delete(email, mailIds)
+        delete.delete(AuthenticateUi.loggedInUser?.email!!, mailIds)
     }
 
     override fun markAsUnRead(mailIds: List<String>) {
-        operations.markUnread(email, mailIds)
+        operations.markUnread(AuthenticateUi.loggedInUser?.email!!, mailIds)
     }
 
     override fun markAsImportant(mailIds: List<String>) {
-        operations.markImportant(email, mailIds)
+        operations.markImportant(AuthenticateUi.loggedInUser?.email!!, mailIds)
     }
 
     override fun markAsUnImportant(mailIds: List<String>) {
-        operations.markUnImportant(email, mailIds)
+        operations.markUnImportant(AuthenticateUi.loggedInUser?.email!!, mailIds)
     }
 
     override fun send(mail: Mail) {
@@ -46,7 +45,7 @@ class MailOperationsUi : MailOperationsUiServices {
 
     override fun reply(mail: Mail, all: Boolean) {
         val newMail =
-            Mail(email, mail.to, mail.subject, "", if (all) mail.CC else listOf(), if (all) mail.BCC else listOf())
+            Mail(AuthenticateUi.loggedInUser?.email!!, mail.to, mail.subject, "", if (all) mail.CC else listOf(), if (all) mail.BCC else listOf())
         newMail.headMailId = mail.headMailId ?: mail.id
         mail.childMailId = newMail.id
         println(newMail)
